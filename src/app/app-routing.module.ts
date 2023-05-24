dc2929584131c5a6057d2d586d8c1f6bd8225f9d
LeonardoPell <leonardojbpellegrino@gmail.com>
2023-05-24 14:06:31 -0300

    feat: botoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EsteirasComponent } from './pages/esteiras/esteiras.component';
+import { BotoesComponent } from './pages/botoes/botoes.component';
 
 const routes: Routes = [
-  {path: '', component: EsteirasComponent}
+  {path: '', component: EsteirasComponent},
+  {path: 'botoes', component: BotoesComponent}
 ];
 
 @NgModule({
--- src/app/pages/botoes/botoes.component.ts
+++ src/app/pages/botoes/botoes.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EquipamentosService } from 'src/app/core/api/esteiras.model';
+import { Equipamento } from 'src/app/core/interfaces/esteira.model';
 
 @Component({
   selector: 'app-botoes',
   templateUrl: './botoes.component.html',
   styleUrls: ['./botoes.component.scss']
 })
-export class BotoesComponent {
+export class BotoesComponent implements OnInit {
+
+  esteiras: Equipamento[] = [];
+
+  constructor(private readonly _equipamentosService: EquipamentosService){}
+
+  ngOnInit(): void {
+    this._equipamentosService.getEquipamentos().subscribe(
+      (equipamentos) => {
+        this.esteiras = equipamentos;
+      }
+    );
+  }
+
+  mudaCorEsteira(event: Equipamento): void{
+    switch (event.cor) {
+      case 'vermelha':
+        event.cor = 'verde';
+        break;
+    
+      default:
+        event.cor = 'vermelha';
+        break;
+    }
+
+    this._equipamentosService.atualizarEquipamento(event.id,event).subscribe((response: Equipamento) => {});
+
+    return;
+  }
 
 }
